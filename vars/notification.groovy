19c15ce3c25f16d5b2caea321f64bf4b01e330da
AMIBYO <ghoulem.mohamed@gmail.com>
2020-06-28 19:20:32 +0200

    Update notification.groovy

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,13 +1,13 @@
 import org.example.Constants
 
 def call(Map config=[:]) {
-        stage("Noification") {
+   
     if (config.type == "slack") {
         echo Constants.SLACK_MESSAGE
         echo config.message
     } else {
         echo Constants.EMAIL_MESSAGE
         echo config.message
-    }
+    
 }
 }
