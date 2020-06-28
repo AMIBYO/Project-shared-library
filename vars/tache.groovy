36523a7a8e207650f260adeb2f6a3cfe4d2e8b3e
AMIBYO <ghoulem.mohamed@gmail.com>
2020-06-28 18:52:48 +0200

    Update tache.groovy

--- vars/tache.groovy
+++ vars/tache.groovy
@@ -7,22 +7,5 @@ def call(Map stageParams) {
         userRemoteConfigs: [[ url: stageParams.url ]]
          
     ])
-     node {
-        git url:  stageParams.url
-
-        stage("Compile") {
-            bat "mvn compile"
-        }
-
-        stage("Test") {
-            bat "mvn test"
-        }
-        stage("install") {
-            bat "mvn install"
-        }
-              stage("package") {
-            bat "mvn package"
-        }
- 
-    }
+     
   }
